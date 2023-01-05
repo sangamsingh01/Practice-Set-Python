8494f1cfe3b0d1736b496ccbdf7f91d3e0769266
sangamsingh01 <121629557+sangamsingh01@users.noreply.github.com>
2023-01-05 06:29:32 +0000

    ReversePantStyle

---
+++ Set/PatternNew.py
@@ -0,0 +1,16 @@
+
+for i in range (1,5,1):
+
+    for j in range(1, i+1, 1):
+        print(j,end = "")
+        
+    
+    for k in range (i, 4, 1):
+        print(" ", end="")
+
+    for l in range (i, 0, -1):
+        print(l, end="")
+
+
+    
+    print("\r")
